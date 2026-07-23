9749f1024c1e8c229e7659eae15ba39ccc09ebc2
znlgis <znlgis@foxmail.com>
2026-07-23 14:49:43 +0800

    feat: add plugin entry point implementing IExtensionApplication

---
+++ src/GStarCad.Net.Demo/ExtensionApplication.cs
@@ -0,0 +1,15 @@
+using GrxCAD.Runtime;
+
+namespace GStarCad.Net.Demo
+{
+    public class ExtensionApplication : IExtensionApplication
+    {
+        public void Initialize()
+        {
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+}
